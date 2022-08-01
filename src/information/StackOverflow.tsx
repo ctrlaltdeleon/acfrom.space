ad58bf1bc8067f0dcf5ed3d5fa9fb5e7745c5785
acfromspace <acfromspace.deleon@gmail.com>
2022-07-31 18:25:58 -0700

    [+] proper fetch api response complete

--- src/information/StackOverflow.tsx
+++ src/information/StackOverflow.tsx
@@ -2,8 +2,20 @@ import React from "react";
 
 export default function StackOverflow() {
   console.log(
-    fetch("https://reqres.in/api/users")
-      .then((result) => result.json())
+    fetch("https://reqres.in/api/users", {
+      method: "POST", // ac***
+      headers: {
+        "Content-Type": "application/json", // ac***
+      },
+      body: JSON.stringify({
+        // ac***
+        name: "User 1",
+      }),
+    })
+      .then((result) => {
+        result.ok ? console.log("SUCCESS!") : console.log("FAILURE!"); // ac***
+        return result.json();
+      })
       .then((data) => console.log(data))
       .catch((error) => console.log("ERROR!"))
   );
