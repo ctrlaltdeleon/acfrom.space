7e974d5c1059a65b20c5455519608cd76721f182
AC De Leon <acfromspace.deleon@gmail.com>
2022-09-01 16:15:47 -0700

    [*] youtube minor issue, but now fixed

--- components/YouTube.tsx
+++ components/YouTube.tsx
@@ -6,7 +6,7 @@ import useSWR from "swr";
 export default function YouTube() {
   const { data } = useSWR<YouTubeModel>("/api/youtube", fetcher);
 
-  const link: string = "https://www.youtube.com/channel/" + data?.linkId;
+  const link = "https://www.youtube.com/channel/" + data?.linkId;
   const subscribers = data?.subscribers;
   const videos = data?.videos;
   const views = data?.views;
